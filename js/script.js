c54bf958ba433329dface756684671e24d6f8e1d
Nassim Abdellaoui <nassim.abdel.85@gmail.com>
2022-11-23 05:46:12 -0500

    removed coordinates

--- js/script.js
+++ js/script.js
@@ -1010,11 +1010,11 @@ var lastTime;
   if (check) check();
 
   //display coordinates
-  info.innerHTML = `<span>X: </span>${mesh.position.x.toFixed(
-		2
-	)}, &nbsp;&nbsp;&nbsp; <span>Y: </span>${mesh.position.y.toFixed(
-		2
-	)}, &nbsp;&nbsp;&nbsp; <span>Z: </span>${mesh.position.z.toFixed(2)}`;
+  // info.innerHTML = `<span>X: </span>${mesh.position.x.toFixed(
+  //	2
+  // )}, &nbsp;&nbsp;&nbsp; <span>Y: </span>${mesh.position.y.toFixed(
+  //	2
+  // )}, &nbsp;&nbsp;&nbsp; <span>Z: </span>${mesh.position.z.toFixed(2)}`;
 
   //flag
   modifier && modifier.apply();
